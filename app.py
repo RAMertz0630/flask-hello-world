9395893883fa55a9ecfe568e795296eaa42c6020
RAMertz0630 <mertz.rachel.a@gmail.com>
2025-11-05 17:15:33 -0500

    rame4494 db_create implemented

--- app.py
+++ app.py
@@ -15,3 +15,23 @@ def db_test():
     
     con.close()
     return 'Database connection successful!'
+
+@app.route('/db_create')
+def db_create():
+    dbURLInt = 'postgresql://rame4494_lab10_db_user:1DyTHdBhetYk8VRr2DrEv2hbVJuqcGxK@dpg-d45s5q7diees738fufdg-a/rame4494_lab10_db'
+    con = psycopg2.connect(dbURLInt)
+    
+    cur = con.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS Basketball(
+        First varchar(255),
+        Last varchar(255),
+        City varchar(255),
+        Name varchar(255),
+        Number int
+        );
+    ''')
+
+    con.commit()
+    con.close()
+    return 'Basketball table successfully created!'
